df0c8c6f09b013728d5f3e5bb1e1b31b478d26e1
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-08-08 16:56:37 +0100

    Update DATU.py

--- DATU.py
+++ DATU.py
@@ -3,6 +3,16 @@ import platform
 import webbrowser
 os.system('termux-setup-storage')
 os.system('git pull')
+try:os.system('mkdir /sdcard/CHI-DATA')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
 P = '\x1b[1;97m'
 import os,requests
 xr = requests.get("http://ip-api.com/json/").json()
@@ -15,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("DATU").login()
+		__import__("XCARET").Main()
 	else:
-		__import__("DATU").login()
+		__import__("XCARET").Main()
