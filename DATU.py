00591979ed3fbe4443a9ebe9bda8230fa3fd0bbe
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-08-08 17:00:37 +0100

    Update DATU.py

--- DATU.py
+++ DATU.py
@@ -25,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("XCARET").Main()
+		__import__("DATU").Main()
 	else:
-		__import__("XCARET").Main()
+		__import__("DATU").Main()
